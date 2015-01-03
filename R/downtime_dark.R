c944e9d7f4acadcbec432283f6f8c250a47654ea
majj <aidear@163.com>
2015-01-03 15:28:23 +0800

    dark theme

---
+++ R/downtime_dark.R
@@ -0,0 +1,64 @@
+
+# downtime graph, Dark Theme
+
+Sys.setlocale("LC_ALL", "C")
+
+library(reshape2)
+library(ggplot2)
+library(scales)
+
+downtime <- function(){
+    DF <- read.table(text=iconv("ID    Equipment    Downtime     Running     Idle
+    1    设备1    1.7    6.15    1
+    2    设备2    1.0    6.6    0.5
+    3    设备3    0.9    7.6    0
+    4    设备4    1.8    6.6    1.5
+    5    设备5    1.5    4.9    2
+    6    设备6    0.45    5.55    1
+    7    设备7    1.25    4.55    2","UTF-8","UTF-8"), header=TRUE)
+
+    #print(DF)
+
+
+
+    DF1 <- melt(DF, id.var=c("ID","Equipment"))
+
+    #print(DF1)
+
+
+
+    plt <- ggplot(DF1, aes(x = ID, y = value, fill = variable )) +   #, fill = variable 
+            
+            geom_bar(stat = "identity", width=0.7,  alpha=0.7,   position="fill")   +
+            
+            #ggtitle(iconv("月停线时间","UTF-8","UTF-8")) + #iconv
+            theme(plot.title = element_text(lineheight=3, face="bold", color="black", size=12) )+           
+            
+            scale_x_continuous(breaks=DF1$ID, labels= DF1$Equipment)  +
+            #scale_y_continuous(breaks=DF1$value) +
+            ylab("时长百分比(%)") +
+            scale_y_continuous(labels = percent) + 
+            
+            scale_fill_manual(values=c("#FF4136", "#2ECC40", "#FFDC00"), 
+                        guide = guide_legend(title = "图例"))  +
+            scale_alpha_manual(values = c(0.2,0.2, 0.2))+
+           
+            theme(axis.text.x = element_text(angle=30, vjust=0.1, size=7, color="#BBBFC2"),
+                    axis.title.x=element_blank(),
+                    axis.text.y = element_text(size=7,  color="#BBBFC2"),
+                    axis.title.y = element_text(size=10,  color="#BBBFC2"),
+                    panel.grid.major=element_line(color="#444444"),
+                    panel.grid.minor=element_line(color="#444444"),
+                    panel.background=element_blank(), 
+                    #panel.border=element_rect(color= "#1F1F1F"),
+                    legend.title = element_text(size=7,  color="#BBBFC2"),
+                    legend.text = element_text(size=7,  color="#BBBFC2"),
+                    legend.background = element_rect(colour = '#1F1F1F', fill = '#1F1F1F'),
+                    plot.background=element_rect(fill = "#1F1F1F", color = "#1F1F1F")
+                    ) 
+      
+    ggsave("output/downtime1.png",width=6, height=2.5)
+
+}
+
+downtime()
---
+++ R/one_month_dark.R
@@ -0,0 +1,73 @@
+
+Sys.setlocale("LC_ALL", "C")
+
+#library(reshape2)
+library(ggplot2)
+
+monthly_stats <- function(DF) {
+
+    #print(DF)
+    #DF1 <- melt(DF, id.var="Date")
+
+    #print(DF1)
+    plt <- ggplot(DF, aes(x = Date, y = F2)) + 
+            ylab("效率(%)") +
+            #ggtitle(iconv("月运行情况","UTF-8","UTF-8")) + #iconv
+            geom_bar(stat = "identity", alpha=0.9, fill="#2ECC40", position="dodge") +
+            scale_x_continuous(breaks=DF$Date, labels=DF$DateString) +  
+            theme(axis.text.x = element_text(angle=90, vjust=0.1, size=6, ,  color="#BBBFC2"),
+            
+               axis.title.x=element_blank(),
+                    axis.text.y = element_text(size=7,  color="#BBBFC2"),
+                    axis.title.y = element_text(size=10,  color="#BBBFC2"),
+                    
+                                panel.grid.major=element_line(color="#444444"),
+                    panel.grid.minor=element_line(color="#444444"),
+                    panel.background=element_blank(), 
+                    #panel.border=element_rect(color= "#1F1F1F"),
+                    legend.title = element_text(size=7,  color="#BBBFC2"),
+                    legend.text = element_text(size=7,  color="#BBBFC2"),
+                    legend.background = element_rect(colour = '#1F1F1F', fill = '#1F1F1F'),
+                    plot.background=element_rect(fill = "#1F1F1F", color = "#1F1F1F")
+            
+            )
+
+    ggsave("output/monthly_stats.png",width=6, height=3)
+    
+}
+
+
+DF <- read.table(text="Date    DateString    F1     F2     F3
+1    2014-12-1    0    41    59
+2    2014-12-2    1    59    62
+3    2014-12-3    2    75    96
+4    2014-12-4    3    93    92
+5    2014-12-5    4    65    95
+6    2014-12-6    5    0    0
+7    2014-12-7    6    0    0
+8    2014-12-8    0    99    50
+9    2014-12-9    1    55    93
+10    2014-12-10    2    51    71
+11    2014-12-11    3    47    82
+12    2014-12-12    4    61    91
+13    2014-12-13    5    0    0
+14    2014-12-14    6    0    0
+15    2014-12-15    0    64    67
+16    2014-12-16    1    61    72
+17    2014-12-17    2    99    69
+18    2014-12-18    3    76    93
+19    2014-12-19    4    98    82
+20    2014-12-20    5    0    0
+21    2014-12-21    6    0    0
+22    2014-12-22    0    59    62
+23    2014-12-23    1    91    86
+24    2014-12-24    2    42    81
+25    2014-12-25    3    96    93
+26    2014-12-26    4    97    80
+27    2014-12-27    5    0    0
+28    2014-12-28    6    0    0
+29    2014-12-29    0    96    55
+30    2014-12-30    1    76    91
+31    2014-12-31    2    97    94", header=TRUE)
+
+monthly_stats(DF)
