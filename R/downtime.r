53e3625e9b5a650ba0090c8d7b147b0b3fb3346d
majj <aidear@163.com>
2015-01-04 15:30:10 +0800

    dark theme

--- R/downtime.r
+++ R/downtime.r
@@ -1,9 +1,12 @@
 
+# downtime graph
+
 Sys.setlocale("LC_ALL", "C")
 
 library(reshape2)
 library(ggplot2)
 library(scales)
+
 
 downtime <- function(){
     DF <- read.table(text=iconv("ID    Equipment    Downtime     Running     Idle
@@ -29,7 +32,7 @@ downtime <- function(){
             
             geom_bar(stat = "identity", width=0.7,  alpha=0.7,   position="fill")   +
             
-            ggtitle(iconv("月停线时间","UTF-8","UTF-8")) + #iconv
+            #ggtitle(iconv("月停线时间","UTF-8","UTF-8")) + #iconv
             theme(plot.title = element_text(lineheight=3, face="bold", color="black", size=12) )+           
             
             scale_x_continuous(breaks=DF1$ID, labels= DF1$Equipment)  +
@@ -39,13 +42,23 @@ downtime <- function(){
             
             scale_fill_manual(values=c("#FF4136", "#2ECC40", "#FFDC00"), 
                         guide = guide_legend(title = "图例"))  +
-            theme(axis.text.x = element_text(angle=30, vjust=0.1, size=7),
+            scale_alpha_manual(values = c(0.2,0.2, 0.2))+
+           
+            theme(axis.text.x = element_text(angle=30, vjust=0.1, size=7, color="#BBBFC2"),
                     axis.title.x=element_blank(),
-                    axis.text.y = element_text(size=7),
-                    axis.title.y = element_text(size=10)
+                    axis.text.y = element_text(size=7,  color="#BBBFC2"),
+                    axis.title.y = element_text(size=10,  color="#BBBFC2"),
+                    panel.grid.major=element_line(color="#444444"),
+                    panel.grid.minor=element_line(color="#444444"),
+                    panel.background=element_blank(), 
+                    #panel.border=element_rect(color= "#1F1F1F"),
+                    legend.title = element_text(size=7,  color="#BBBFC2"),
+                    legend.text = element_text(size=7,  color="#BBBFC2"),
+                    legend.background = element_rect(colour = '#1F1F1F', fill = '#1F1F1F'),
+                    plot.background=element_rect(fill = "#1F1F1F", color = "#1F1F1F")
                     ) 
       
-    ggsave("output/downtime.png",width=6, height=3)
+    ggsave("output/downtime1.png",width=6, height=2.5)
 
 }
 
--- R/one_month.r
+++ R/one_month.r
@@ -12,12 +12,27 @@ monthly_stats <- function(DF) {
     #print(DF1)
     plt <- ggplot(DF, aes(x = Date, y = F2)) + 
             ylab("效率(%)") +
-            ggtitle(iconv("月运行情况","UTF-8","UTF-8")) + #iconv
+            #ggtitle(iconv("月运行情况","UTF-8","UTF-8")) + #iconv
             geom_bar(stat = "identity", alpha=0.9, fill="#2ECC40", position="dodge") +
             scale_x_continuous(breaks=DF$Date, labels=DF$DateString) +  
-            theme(axis.text.x = element_text(angle=90, vjust=0.1, size=6))
+            theme(axis.text.x = element_text(angle=90, vjust=0.1, size=6, ,  color="#BBBFC2"),
+            
+               axis.title.x=element_blank(),
+                    axis.text.y = element_text(size=7,  color="#BBBFC2"),
+                    axis.title.y = element_text(size=10,  color="#BBBFC2"),
+                    
+                                panel.grid.major=element_line(color="#444444"),
+                    panel.grid.minor=element_line(color="#444444"),
+                    panel.background=element_blank(), 
+                    #panel.border=element_rect(color= "#1F1F1F"),
+                    legend.title = element_text(size=7,  color="#BBBFC2"),
+                    legend.text = element_text(size=7,  color="#BBBFC2"),
+                    legend.background = element_rect(colour = '#1F1F1F', fill = '#1F1F1F'),
+                    plot.background=element_rect(fill = "#1F1F1F", color = "#1F1F1F")
+            
+            )
 
-    ggsave("output/monthly_stats.png",width=6, height=4)
+    ggsave("output/monthly_stats.png",width=6, height=3)
     
 }
 
